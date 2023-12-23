4e2e1d5eb0c85ee721389a5e6a1c9fd87ad57591
arkhamknight0878 <147205305+arkhamknight0878@users.noreply.github.com>
2023-12-23 14:05:37 +0300

    Delete Matrix directory

--- Matrix/final.h
+++
@@ -1,7 +0,0 @@
-/**
- * @brief : Поиско обратной матрицы
- * @param matrix : Исходная матрица
- * @param reverse_ : Мартица, обратная к исходной
- * @param size_ : Размер исходной матрицы
-*/
-void matrix_reverse(int** matrix, int** reverse_, unsigned size_);
