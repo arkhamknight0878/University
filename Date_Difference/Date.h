0c2248ee868bdaa7e9c5086d21dd084609f8b66b
arkhamknight0878 <147205305+arkhamknight0878@users.noreply.github.com>
2024-02-16 20:19:07 +0300

    Add files via upload

---
+++ Date_Difference/Date.h
@@ -0,0 +1,10 @@
+#pragma once
+
+typedef struct Date
+{
+	unsigned year;    // Год
+	unsigned mounth;  // Месяц
+	unsigned day;     // День
+	unsigned hour;    // Час
+	unsigned minuet;  // Минуты
+} Date;
