80fa4e509b677014fcb69e912eb49a0f17f8d091
arkhamknight0878 <147205305+arkhamknight0878@users.noreply.github.com>
2024-04-20 21:09:09 +0300

    Delete Euler3 directory

--- Euler3/functions.h
+++
@@ -1,8 +0,0 @@
-#include <stdbool.h>
-
-/**
- * @brief : Проверка числа на простоту
- * @param divisor_ : Проверяемое число
- * @return : true если число простое; false иначе
-*/
-bool Is_Prime(unsigned long long divisor_);
