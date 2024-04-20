97ca9275a9a4445e794e7317850117a5b8aeb66b
arkhamknight0878 <147205305+arkhamknight0878@users.noreply.github.com>
2024-04-20 19:29:43 +0300

    Delete Project Euler/Euler 1 directory

--- Project Euler/Euler 1/Function.h
+++
@@ -1,6 +0,0 @@
-/**
- * @brief : Считает сумму чисел, меньших заданного и кратных трём и пяти
- * @param upper_bound_ : Максимальное число
- * @return : Сумма чисел, меньших заданного и кратных трём и пяти
-*/
-unsigned long long Sum_of_Mult35(unsigned long long upper_bound_);
