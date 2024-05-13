6a4a374d7da7ec74b94c0f739b6673efa029a650
arkhamknight0878 <147205305+arkhamknight0878@users.noreply.github.com>
2024-05-13 12:37:40 +0300

    Delete Problem_4 directory

--- Problem_4/Euler.h
+++
@@ -1,2 +0,0 @@
-
-unsigned long long Problem_4(unsigned long long MaxPol_);
--- Problem_4/Function.h
+++
@@ -1,5 +0,0 @@
-#include<stdbool.h>
-
-unsigned long long Reverse(unsigned long long Num_);
-
-bool Is_Polin(unsigned long long Rev_Num_);
